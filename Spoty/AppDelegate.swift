e7ecf6127ff814524d1de23fb50b2788ddd96631
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-05-20 14:56:38 +0200

    Setup project.

---
+++ Spoty/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Spoty
+//
+//  Created by Jelle Vandenbeeck on 20/05/15.
+//  Copyright (c) 2015 Fousa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+

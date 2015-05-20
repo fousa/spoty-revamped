5e8852c4f8a20735ee0dda8a96559a0c4a54dd03
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-05-20 16:09:44 +0200

    Refresh competitions.

--- Spoty/Modules/CompetitionSearchViewController.swift
+++ Spoty/Modules/CompetitionSearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  CompetitionSearchTableViewController.swift
+//  CompetitionSearchViewController.swift
 //  Spoty
 //
 //  Created by Jelle Vandenbeeck on 20/05/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CompetitionSearchTableViewController: UIViewController {
+class CompetitionSearchViewController: UIViewController {
     
     // MARK: - Outlets
     
@@ -17,21 +17,37 @@ class CompetitionSearchTableViewController: UIViewController {
     // MARK: - Properties
     
     private var competitions: [Competition]?
+    private var refreshControl: UIRefreshControl!
 
     // MARK: - View flow
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "reloadData", forControlEvents: .ValueChanged)
+        tableView.addSubview(refreshControl)
+    }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
+        reloadData()
+    }
+    
+    // MARK: - Data
+    
+    func reloadData() {
         ServiceClient().fetchRecentCompetitions { (response, error) -> () in
             self.competitions = response as? [Competition]
             self.tableView.reloadData()
+            self.refreshControl.endRefreshing()
         }
     }
 
 }
 
-extension CompetitionSearchTableViewController: UITableViewDataSource {
+extension CompetitionSearchViewController: UITableViewDataSource {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return competitions?.count ?? 0
